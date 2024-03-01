515c19d8715367c1fa80141b351124c3133ae0af
dhillonjot <160789207+dhillonjot@users.noreply.github.com>
2024-03-01 14:13:50 +1300

    Add files via upload

---
+++ triangle activity.py
@@ -0,0 +1,90 @@
+print("*")
+
+print("**") 
+
+print("***") 
+print("****")
+print("*****")
+
+print("The sum of 12 and 5.7 are as follows:")
+print(12+5.7)
+
+# Testing whether 2 is greater than 1 print(2>1)
+""" 
+I’m using these as multi-line comments.
+I use these lines for your reference only.
+
+
+I use these lines for your reference only.
+These are common in class, functions, or modules.
+"""
+"""
+
+I’m trying to use multi-line comments using three double-quotes.
+I use these lines for your reference only.
+These are common in class, functions, or modules. 
+"""
+# Now I'm showing multiple single-line comments
+# Multiplying 2 with 3 # And printing the result
+print(2*3)
+
+print("Hello World!")
+my_text = "Hello World!" 
+
+print(my_text)  
+
+print("This is Sam's book.")
+
+print(1)   #Prints 1
+print(5.7)   # Prints 5.7 
+
+print(-6.789)   # Prints -6.789
+
+my_int=125 
+print(my_int)  # Prints 125 
+my_float=25.763 
+print(my_float)  # Prints 25.763
+
+
+# Difference between the numbers and the strings 
+print("1"+ "2")  #Prints 12 
+print(1+2)  #Prints 3
+
+print("*** This program shows the use of a variable. ***")
+
+# mytext is holding the value "Hello World!"
+print(my_text)
+# my_text is holding a new value
+
+my_text = "Dear Reader, how are you?" 
+print(my_text)
+
+number1=2
+number2=3
+print(number1+number2)
+
+number1=2.0
+print(number1+number2)
+
+number1=2
+print (type(number1))
+number1=2.0
+print (type(number1))
+
+student_1="John"
+print(student_1)
+student2="Sam"
+print(student2)
+student="Kate"
+print("3student")
+
+x,y,z = 10,25,-303.5
+print(x)
+print(y)
+
+print(z)
+
+
+
+
+
